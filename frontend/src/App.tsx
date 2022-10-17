9b051881523798279e17ce11b30c8d2d34c0cc8d
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-10-16 22:23:22 -0400

    fix: cleanup debug and fix all Dockerfiles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-// import { format } from 'date-fns'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +8,6 @@ function App() {
       <div>Hello World</div>
       <button onClick={() => setCount(count + 1)}>
         Count is <span className={'tabular-nums'}>{count}</span>
-        {/* {format(new Date(), "'Today is a' eeee")} */}
       </button>
     </div>
   )
