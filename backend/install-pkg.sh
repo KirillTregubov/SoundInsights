14e0c7bc1c35fc20d9bfeb6fea5569c5c125b86f
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-10-17 00:01:48 -0400

    fix: install-pkg.sh printf

--- backend/install-pkg.sh
+++ backend/install-pkg.sh
@@ -11,12 +11,18 @@ then
   exit
 fi
 
+current_dir=`basename $PWD`
+
 if pip3 install $1 >/dev/null 2>&1; then
   pip3 freeze | grep $1 >> requirements.txt
   printf "\033[32mSuccessfully installed \033[1;36m$1\033[0m\n"
   printf "\033[32mAdded \033[1;36m$1\033[32m to requirements.txt\033[0m\n"
   if grep -qi microsoft /proc/version; then
-    printf "\033[33mThe script has detected that you are running in WSL, please execute \033[1;36mpip3 install -r requirements.txt\033[0m\n"
+    str="\033[33mThe script has detected that you are running in WSL, please execute \033[1;36mpip3 install -r requirements.txt\033[0m\n"
+    if ! [ "$current_dir" = "backend" ]; then
+      str="\033[33mThe script has detected that you are running in WSL, please execute \033[1;36mcd backend\033[33m followed by \033[1;36mpip3 install -r requirements.txt\033[0m\n"
+    fi
+    printf "$str"
   fi
 else
   printf "\033[31mError: Failed to install \033[1;36m$1\033[31m, please ensure it's a valid PyPi package\033[0m\n" >&2
