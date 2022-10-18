0bfb6acdb337560b13dd93347868f8d732769111
Eddie Shao <shao.eddie@gmail.com>
2022-10-18 15:23:16 -0400

    added testing with broken imports

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,24 +3,24 @@ from flask_cors import CORS
 from db_demo import db_demo
 from db_helper import close_db
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
 
+    @app.teardown_appcontext
+    def cleanup(exception):
+        close_db()
 
-@app.teardown_appcontext
-def cleanup(exception):
-    close_db()
+    @app.route("/")
+    def hello_world():
+        response = make_response(jsonify({"message": "Hello, World!"}), 200)
+        response.headers["Content-Type"] = "application/json"
+        return response
 
-
-@app.route("/")
-def hello_world():
-    response = make_response(jsonify({"message": "Hello, World!"}), 200)
-    response.headers["Content-Type"] = "application/json"
-    return response
-
-
-@app.route("/db-demo")
-def query_db():
-    response = db_demo()
-    response.headers["Content-Type"] = "application/json"
-    return response
+    @app.route("/db-demo")
+    def query_db():
+        response = db_demo()
+        response.headers["Content-Type"] = "application/json"
+        return response
+    
+    return app
---
+++ backend/tests/test_main.py
@@ -0,0 +1,26 @@
+import pytest
+from src.main import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_hello_world(client):
+	response = client.get("/")
+	assert "message" in response.json.keys()
+	assert response.json["message"] == "Hello, World!"
+
+def test_query_db(client):
+	response = client.get("/db-demo")
+	keys = response.json.keys()
+	assert "query" in keys
+	assert "result" in keys
