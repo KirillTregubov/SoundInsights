9a3f707deb85dd44c2898d4bf4c61ba7733cf8ba
Eddie Shao <shao.eddie@gmail.com>
2022-10-23 14:41:35 -0400

    added more items to track_uris to match target input

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -26,7 +26,13 @@ def test_query_db(client):
 
 def test_recommend_tracks_endpoint(client):
     response = client.post("/recommend-tracks", data={
-        "data": ["0UaMYEvWZi0ZqiDOoHU3YI", "6I9VzXrHxO9rA9A5euc8Ak"]
+        "data": [
+			"0UaMYEvWZi0ZqiDOoHU3YI",
+			"6I9VzXrHxO9rA9A5euc8Ak",
+			"0WqIKmW4BTrj3eJFmnCKMv",
+			"1AWQoqb9bSvzTjaLralEkT",
+			"1lzr43nnXAijIGYnCT8M8H"
+		]
     })
     assert response.status_code == 200
     assert isinstance(response.json, List)
