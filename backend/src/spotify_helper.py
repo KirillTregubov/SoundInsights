e02b4b93583be86d0623a2b8fa784ff146755248
Eddie Shao <shao.eddie@gmail.com>
2022-10-23 14:30:13 -0400

    move env vars to docker yml

--- backend/src/spotify_helper.py
+++ backend/src/spotify_helper.py
@@ -1,12 +1,7 @@
 from typing import Optional
 import requests
 import os
-from dotenv import load_dotenv
 
-
-# Make sure you have a .env file with the correct CLIENT_ID and CLIENT_SECRET to use the Spotify API.
-# If you don't have the correct .env, you get it from our project Discord chat's pinned messages.
-load_dotenv()
 
 def get_access_token() -> Optional[str]:
     response = requests.post("https://accounts.spotify.com/api/token", {
