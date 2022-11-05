2ab2b68baf209a2fdf4416e81f5a8b39c15600e4
Eddie Shao <shao.eddie@gmail.com>
2022-11-04 20:02:58 -0400

    add secret injection to scripts

--- run-tests.sh
+++ run-tests.sh
@@ -15,6 +15,8 @@ clean_up () {
 }
 trap clean_up INT EXIT
 
+export CLIENT_ID=$1
+export CLIENT_SECRET=$2
 printf "\033[32mCreating and starting \033[1;36mDocker \033[0;32mcontainers\033[0m\n"
 printf "\033[33mTip: Use \033[1mCtrl + C\033[0;33m to cancel or close \033[1;36mDocker\033[0m\n\n"
 docker compose --file docker-compose-tests.yml up --build
