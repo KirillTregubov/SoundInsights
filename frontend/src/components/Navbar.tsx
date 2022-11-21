20c6927c9d5ee6447c9b167a636280f618843bfa
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-11-21 04:39:54 -0500

    fix: navbar clipping

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="sticky top-0 z-0 -mb-9 bg-gradient-to-b from-neutral-900 via-neutral-900 p-2 pb-9">
-      <Link to="/" className="z-10 flex items-center gap-2">
+    <nav className="pointer-events-none sticky top-0 z-10 -mb-9 bg-gradient-to-b from-neutral-900 via-neutral-900 p-2 pb-9">
+      <Link to="/" className="pointer-events-auto z-10 flex items-center gap-2">
         <img
           className="h-7 w-7 select-none"
           src="/assets/icon.svg"
--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -5,7 +5,7 @@ const Root: React.FC<{ children?: JSX.Element }> = ({ children }) => {
   return (
     <div id="App" className="">
       <Navbar />
-      <div className="relative z-10 my-6 mx-2">{children || <Outlet />}</div>
+      <div className="my-6 mx-2">{children || <Outlet />}</div>
     </div>
   )
 }
