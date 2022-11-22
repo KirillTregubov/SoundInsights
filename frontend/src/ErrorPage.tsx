818f63d42ef1e887c5ce52dbec4bc0943878f661
Kirill Tregubov <31662934+KirillTregubov@users.noreply.github.com>
2022-11-22 17:22:16 -0500

    fix: error page link styling

--- frontend/src/ErrorPage.tsx
+++ frontend/src/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import { Link, useRouteError } from 'react-router-dom'
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 interface ErrorType {
   status?: number
@@ -17,8 +18,13 @@ const ErrorPage: React.FC = () => {
           : `Error ${error.status}: ${error.statusText}` ||
             error.message ||
             'Something went wrong.'}
-        <Link to="/">Go Home</Link>
       </h1>
+      <Link
+        to="/"
+        className="group mt-1 inline-flex items-center rounded-md bg-neutral-800 py-1 px-1 pl-2 font-medium hover:bg-neutral-700">
+        Go Home
+        <ChevronRightIcon className="h-4 w-4 transition-transform will-change-transform group-hover:translate-x-0.5" />
+      </Link>
     </div>
   )
 }
