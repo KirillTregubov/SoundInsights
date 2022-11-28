de56629bdcc66c9e4d0eeedd24ebc9ca6cef6721
Eddie Shao <shao.eddie@gmail.com>
2022-11-28 17:36:00 -0500

    add create dir and file to init logging

--- init-logging.sh
+++ init-logging.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Run this in the beginning of your shell script to copy its output to a log file in the logs directory.
 
+mkdir script-logs
+touch ./script-logs/$1
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1> >(tee ./script-logs/$1) 2>&1
